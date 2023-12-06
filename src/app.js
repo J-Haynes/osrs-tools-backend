9a7631d0b05928bc189bbd407fe3d5231fdcaa26
Jack Haynes <haynes.jack.stewart@gmail.com>
2023-12-06 15:22:24 +1300

    adds /proxy route

--- src/app.js
+++ src/app.js
@@ -1,29 +1,42 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 
-require('dotenv').config();
+const axios = require('axios')
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+require('dotenv').config()
 
-const app = express();
+const middlewares = require('./middlewares')
+const api = require('./api')
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
+  })
+})
 
-app.use('/api/v1', api);
+app.get('/proxy', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://oldschool.runescape.com/slu?order=WMLPA'
+    )
+    res.json(response.data)
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data' })
+  }
+})
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use('/api/v1', api)
 
-module.exports = app;
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
+
+module.exports = app
